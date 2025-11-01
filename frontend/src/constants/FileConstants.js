04554c6b75ab23a081470e35dc712c8e78926542
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2025-11-01 23:19:17 +0530

    Package updates complete till now - many files are added in gitIgnore - Constant file is made to store all the constant data

---
+++ frontend/src/constants/FileConstants.js
@@ -0,0 +1,86 @@
+export const products = [
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+  {
+    title: "Cat",
+    link: "https://mrsaxena.arisertradco.com",
+    thumbnail:
+      "https://ik.imagekit.io/jarvisai/My_Portfolio/Pinterest_Images/20240222074553_IMG_6418.jpg?updatedAt=1755274080575",
+  },
+];
