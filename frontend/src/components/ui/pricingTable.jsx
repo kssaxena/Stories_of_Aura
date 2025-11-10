22f9ec6c236a912e9a20ca9722f25bec9cfafe46
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2025-11-10 17:24:09 +0530

    feat: Add PricingTable component for package selection; implement custom plan modal and enhance UI with responsive design

---
+++ frontend/src/components/ui/pricingTable.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import Button from "../button-wrapper";
+import CustomPlanModal from "./pricingTableCustom";
+
+const PricingTable = ({ packageData }) => {
+  const [showCustom, setShowCustom] = useState(false);
+  //   <CustomPlanModal isOpen={showCustom} onClose={() => setShowCustom(false)} />;
+  return (
+    <div className="w-full py-16 flex flex-col justify-center items-center">
+      <div className="max-w-7xl w-full grid md:grid-cols-3 gap-8 px-4">
+        {packageData?.map((pkg, index) => (
+          <div
+            key={index}
+            className={`
+              relative border rounded-3xl p-8 shadow-sm 
+              hover:shadow-xl transition-all duration-300
+              ${pkg.popular ? "border-red-600 shadow-xl scale-[1.03]" : ""}
+            `}
+          >
+            {/* MOST POPULAR TAG */}
+            {pkg.popular && (
+              <div className="absolute -top-4 left-1/2 -translate-x-1/2 bg-red-600 text-white text-xs font-bold px-4 py-1 rounded-full shadow-md">
+                MOST POPULAR
+              </div>
+            )}
+
+            {/* DISCOUNT TAG */}
+            <div
+              className={`
+                absolute top-4 right-4 text-xs font-semibold px-3 py-1 rounded-full
+                ${
+                  pkg.popular
+                    ? "bg-red-600 text-white"
+                    : "bg-red-100 text-red-700"
+                }
+              `}
+            >
+              {pkg.badge}
+            </div>
+
+            {/* Title */}
+            <h2 className="text-2xl font-bold mb-1">{pkg.name}</h2>
+            <p className=" mb-4">{pkg.subtitle}</p>
+
+            {/* Price */}
+            <div className="text-4xl font-extrabold mb-2">{pkg.price}</div>
+            <div className="text-sm  mb-6">{pkg.occasional}</div>
+
+            {/* Button */}
+            <div className="w-full mb-6 flex justify-center items-center">
+              <Button Label="Collaborate with us" />
+            </div>
+
+            {/* Features */}
+            <div className="space-y-4 border-t pt-6">
+              {pkg.features.map((feature, i) => (
+                <div key={i} className="flex items-start gap-3">
+                  <span className="text-green-600 font-bold">✓</span>
+                  <p className="text-sm">
+                    <span className="font-semibold">{feature.label}:</span>{" "}
+                    {feature.value === true ? "Included" : feature.value}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* COMPARE BUTTON */}
+      <div className=" mt-12">
+        <Button
+          Label="Choose your desired package"
+          onClick={() => setShowCustom(true)}
+        />
+      </div>
+      {showCustom && (
+        <div>
+          <CustomPlanModal
+            isOpen={showCustom}
+            onClose={() => setShowCustom(false)}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PricingTable;
--- frontend/src/pages/aura-experience/page.jsx
+++ frontend/src/pages/aura-experience/page.jsx
@@ -12,6 +12,7 @@ import { AccordionData } from "../../constants/FileConstants";
 const AuraExperience = () => {
   const ref = useRef(null);
   const ref2 = useRef(null);
+  const ref3 = useRef(null);
 
   // Scroll for first motion section
   const { scrollYProgress: scrollY1 } = useScroll({
@@ -24,13 +25,19 @@ const AuraExperience = () => {
     target: ref2,
     offset: ["start end", "end start"],
   });
+  const { scrollYProgress: scrollY3 } = useScroll({
+    target: ref3,
+    offset: ["start end", "end start"],
+  });
 
   // Parallax movement
   const y = useTransform(scrollY1, [0, 1], ["1000px", "-1000px"]);
   const y2 = useTransform(scrollY2, [0, 1], ["1000px", "-1000px"]);
+  const y3 = useTransform(scrollY2, [0, 1], ["500px", "-500px"]);
 
   const opacity = useTransform(scrollY1, [0, 0.2, 0.8, 1], [0, 1, 1, 0.8]);
   const opacity2 = useTransform(scrollY2, [0, 0.2, 0.8, 1], [0, 1, 1, 0.8]);
+  const opacity3 = useTransform(scrollY2, [0, 0.2, 0.8, 1], [0, 1, 1, 0.8]);
   return (
     <div className="flex justify-center items-center flex-col pb-20">
       <motion.div
@@ -84,6 +91,7 @@ const AuraExperience = () => {
         ref={ref2}
         style={{ y2, opacity2 }}
         transition={{ type: "spring", stiffness: 60, damping: 100 }}
+        className="w-full relative"
       >
         {/* accordion section  */}
         {AccordionData.map((item, index) => (
@@ -95,6 +103,15 @@ const AuraExperience = () => {
             description={item.description}
           />
         ))}
+        <div className="absolute top-0 left-0 z-0 opacity-5 grayscale-75 w-full h-full bg-cyan-500">
+          <motion.img
+            ref={ref3}
+            style={{ y3, opacity3 }}
+            transition={{ type: "spring", stiffness: 60, damping: 100 }}
+            className="object-cover sticky top-0"
+            src={`https://ik.imagekit.io/krb6qpkbp/Footer/footer_bg.jpg?updatedAt=1762334189213`}
+          />
+        </div>
       </motion.div>
     </div>
   );
